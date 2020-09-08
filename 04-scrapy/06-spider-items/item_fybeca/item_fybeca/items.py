45d0263bb7ddf9dabbbe9417d2ac3a1249098596
Jair <jair.quinonez@epn.edu.ec>
2020-09-07 19:38:22 -0500

    scrapy

---
+++ 04-scrapy/06-spider-items/item_fybeca/item_fybeca/items.py
@@ -0,0 +1,57 @@
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://docs.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+from scrapy.loader.processors import MapCompose
+from scrapy.loader.processors import TakeFirst
+
+
+##src="../../images/thumbnail/294930.jpg"
+##https://www.fybeca.com/images/thumbnail/294930.jpg
+def tranformar_url_imagen(texto):
+    url_fybeca = 'https://www.fybeca.com'
+    cadena_texto = '../..'
+    return texto.replace(cadena_texto,url_fybeca)
+
+class ProductoFybeca(scrapy.Item):
+    titulo = scrapy.Field()
+    imagen = scrapy.Field(
+        input_processor = MapCompose(
+            #lista de funciones
+            tranformar_url_imagen
+        ),
+        outpu_processor = TakeFirst() #obtenemos una lista[]
+        #Sacamos el primero de la lista
+        
+    )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class ItemFybecaItem(scrapy.Item):
+    # define the fields for your item here like:
+    # name = scrapy.Field()
+    pass
